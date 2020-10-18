20ef0d0129a2bcd9af76e932ca1fdb41a4bb7699
Ankush Dua <ankushduacodes@gmail.com>
2020-10-18 07:13:12 +0530

    changed function name to better reflect what function does

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import selenium
 from flask import Flask, jsonify
 from flask_restx import Api, Resource
-from source import make_source_file
+from source import fetch_html_source_file
 from scraper.forex_scraper import ForexScraper
 from selenium.common.exceptions import TimeoutException
 
@@ -14,7 +14,7 @@ class ForexInfo(Resource):
     def get(self):
         try:
             with open('euro.html', 'w+') as file:
-                file.write(make_source_file())
+                file.write(fetch_html_source_file())
         except NotADirectoryError:
             return jsonify({'Message': 'Please make sure you have chromedriver installed and the path to the driver is'
                                        ' correct'})
--- source.py
+++ source.py
@@ -5,12 +5,13 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 
-def make_source_file():
+def fetch_html_source_file():
     # Change the path to the driver accordingly
     path = os.getenv('path')
     driver = webdriver.Chrome(path)
 
     driver.get("https://www.investing.com/currencies/single-currency-crosses")
     driver.find_element_by_xpath("//*[@id='symbols']/option[40]").click()
+    # using value so even if a new currecy is added the option number will chnage but not the value
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "pair_6")))
     return driver.page_source
