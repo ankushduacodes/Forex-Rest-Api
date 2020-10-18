e3932a7351f04f4d04d5509cda5a13531f4f2768
Ankush Dua <ankushduacodes@gmail.com>
2020-10-18 07:12:11 +0530

    refactored code to reduce code duplicacy

--- scraper/forex_scraper.py
+++ scraper/forex_scraper.py
@@ -5,7 +5,6 @@ class ForexScraper:
 
     def __init__(self):
         self.source = open('./euro.html', 'r')
-        self.info = {}
 
     def get_row(self, tbody, pair):
         return tbody.find('tr', attrs={'id': pair})
@@ -41,7 +40,6 @@ class ForexScraper:
 
     def get_timestamp(self, row, class_str):
         class_str = class_str.split()
-        print(row.find('td', class_=class_str).get_text())
         return row.find('td', class_=class_str).get('data-value')
 
     def get_currency_info(self, row, pid):
@@ -56,42 +54,29 @@ class ForexScraper:
             'timestamp': int(self.get_timestamp(row, f'pid-{pid}-time'))
         }
 
+    def get_currency_info_dict(self, currency_data_tuple_list, tbody):
+        info_dict = {}
+        for currency_data_tuple in currency_data_tuple_list:
+            row = self.get_row(tbody, pair=f'pair_{currency_data_tuple[1]}')
+            print(row)
+            print(type(row))
+            print(currency_data_tuple)
+            info_dict.update({currency_data_tuple[0]: self.get_currency_info(row, currency_data_tuple[1])})
+        return info_dict
+
     def get_info(self):
         soup = BeautifulSoup(self.source, features='html5lib')
         tbody = soup.find('div', attrs={'id': 'cross_rates_container'}).find('tbody')
-
-        usd_row = self.get_row(tbody, 'pair_1')
-        usd_pid = 1
-        euro_row = self.get_row(tbody, 'pair_2124')
-        euro_pid = 2124
-        jyp_row = self.get_row(tbody, 'pair_9')
-        jyp_pid = 9
-        gbp_row = self.get_row(tbody, 'pair_6')
-        gbp_pid = 6
-        aud_row = self.get_row(tbody, 'pair_15')
-        aud_pid = 15
-        cad_row = self.get_row(tbody, 'pair_16')
-        cad_pid = 16
-        chf_row = self.get_row(tbody, 'pair_10')
-        chf_pid = 10
-        sek_row = self.get_row(tbody, 'pair_61')
-        sek_pid = 61
-        nzd_row = self.get_row(tbody, 'pair_52')
-        nzd_pid = 52
-        inr_row = self.get_row(tbody, 'pair_1646')
-        inr_pid = 1646
-
-        self.info = {
-            'EUR/USD': self.get_currency_info(usd_row, usd_pid),
-            'USD/EUR': self.get_currency_info(euro_row, euro_pid),
-            'EUR/JYP': self.get_currency_info(jyp_row, jyp_pid),
-            'EUR/GBP': self.get_currency_info(gbp_row, gbp_pid),
-            'EUR/AUD': self.get_currency_info(aud_row, aud_pid),
-            'EUR/CAD': self.get_currency_info(cad_row, cad_pid),
-            'EUR/CHF': self.get_currency_info(chf_row, chf_pid),
-            'EUR/SEK': self.get_currency_info(sek_row, sek_pid),
-            'EUR/NZD': self.get_currency_info(nzd_row, nzd_pid),
-            'EUR/INR': self.get_currency_info(inr_row, inr_pid)
-        }
-
-        return self.info
+        required_currency_data_tuple_list = [
+            ('EUR/USD', 1),
+            ('USD/EUR', 2124),
+            ('EUR/JYP', 9),
+            ('EUR/GBP', 6),
+            ('EUR/AUD', 15),
+            ('EUR/CAD', 16),
+            ('EUR/CHF', 10),
+            ('EUR/SEK', 61),
+            ('EUR/NZD', 52),
+            ('EUR/INR', 1646)
+        ]
+        return self.get_currency_info_dict(required_currency_data_tuple_list, tbody)
